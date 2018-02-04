13895a81c352b5f6fe37d470a8ca7aaf634b086b
Qais Patankar <qaisjp@gmail.com>
2018-02-04 17:31:34 +0000

    Rename EXPIRACY to EXPIRY

--- app/hackathon_variables.py
+++ app/hackathon_variables.py
@@ -48,7 +48,7 @@ HACKATHON_DEV_EMAILS = ['me@qaisjp.com']
 # Reimbursement configuration
 REIMBURSEMENT_ENABLED = True
 CURRENCY = '£'
-REIMBURSEMENT_EXPIRACY_DAYS = 5
+REIMBURSEMENT_EXPIRY_DAYS = 5
 REIMBURSEMENT_REQUIREMENTS = 'You have to submit a project and demo it during the event in order to be reimbursed.'
 REIMBURSEMENT_DEADLINE = timezone.datetime(2018, 2, 24, 3, 14, tzinfo=timezone.pytz.timezone(TIME_ZONE))
 
