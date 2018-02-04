1d901fe2bd5c5e74d6ea02979bfee2c888683b1e
Qais Patankar <qaisjp@gmail.com>
2018-02-04 18:52:39 +0000

    Tweak strings

--- applications/forms.py
+++ applications/forms.py
@@ -21,15 +21,15 @@ class ApplicationForm(BetterModelForm):
     site = forms.CharField(required=False, widget=forms.TextInput(
         attrs={'class': 'form-control', 'placeholder': 'https://biene.space'}))
     phone_number = forms.CharField(required=False, widget=forms.TextInput(
-        attrs={'class': 'form-control', 'placeholder': '+#########'}))
+        attrs={'class': 'form-control', 'placeholder': '+############'}))
     university = forms.CharField(required=True,
                                  label='What university are you studying at?',
                                  help_text='Current or most recent school you attended.',
                                  widget=forms.TextInput(
                                      attrs={'class': 'typeahead-schools', 'autocomplete': 'off'}))
 
-    degree = forms.CharField(required=True, label='What\'s your Major?',
-                             help_text='Current or most recent degree you\'ve received',
+    degree = forms.CharField(required=True, label='What\'s your degree?',
+                             help_text='Current or most recent degree you\'ve received.',
                              widget=forms.TextInput(
                                  attrs={'class': 'typeahead-degrees', 'autocomplete': 'off'}))
 
@@ -43,7 +43,7 @@ class ApplicationForm(BetterModelForm):
 
     reimb = forms.TypedChoiceField(
         required=False,
-        label='Do you need travel reimbursement to attend?',
+        label='Will you need a travel reimbursement to attend?',
         coerce=lambda x: x == 'True',
         choices=((False, 'No'), (True, 'Yes')),
         initial=False,
@@ -60,8 +60,8 @@ class ApplicationForm(BetterModelForm):
     )
 
     code_conduct = forms.BooleanField(required=False,
-                                      label='I have read and accept '
-                                            '<a href="/code_conduct" target="_blank">%s Code of conduct</a>' % (
+                                      label='I have read and accept the '
+                                            '<a href="/code_conduct" target="_blank">%s Code of Conduct</a>' % (
                                                 settings.HACKATHON_NAME), )
 
     def clean_resume(self):
@@ -78,7 +78,7 @@ class ApplicationForm(BetterModelForm):
         # self.instance.pk is None if there's no Application existing before
         # https://stackoverflow.com/questions/9704067/test-if-django-modelform-has-instance
         if not cc and not self.instance.pk:
-            raise forms.ValidationError("In order to apply and attend you have to accept our code of conduct")
+            raise forms.ValidationError("You must accept our code of conduct.")
         return cc
 
     def clean_github(self):
@@ -145,20 +145,20 @@ class ApplicationForm(BetterModelForm):
         ]
         deadline = getattr(settings, 'REIMBURSEMENT_DEADLINE', False)
         if deadline and deadline <= timezone.now() and not self.instance.pk:
-            self._fieldsets.append(('Traveling',
+            self._fieldsets.append(('Travelling',
                                     {'fields': ('origin',),
                                      'description': 'Reimbursement applications are now closed. '
                                                     'Sorry for the inconvenience.',
                                      }))
         elif self.instance.pk:
-            self._fieldsets.append(('Traveling',
+            self._fieldsets.append(('Travelling',
                                     {'fields': ('origin',),
                                      'description': 'If you applied for reimbursement see it on the Travel tab. '
                                                     'Email us at %s for any change needed on reimbursements.' %
                                                     settings.HACKATHON_CONTACT_EMAIL,
                                      }))
         else:
-            self._fieldsets.append(('Traveling',
+            self._fieldsets.append(('Travelling',
                                     {'fields': ('origin', 'reimb', 'reimb_amount'), }), )
 
         # Fields that we only need the first time the hacker fills the application
@@ -172,11 +172,11 @@ class ApplicationForm(BetterModelForm):
         help_texts = {
             'gender': 'This is for demographic purposes. Feel free to skip this '
                       'question.',
-            'graduation_year': 'What year have you graduated on or when will '
-                               'you graduate',
-            'degree': 'What\'s your major?',
-            'other_diet': 'Please fill here your dietary restrictions. We want to make sure we have food for you!',
-            'lennyface': 'tip: you can chose from here <a href="http://textsmili.es/" target="_blank">'
+            'graduation_year': 'What year did you graduate or when will '
+                               'you graduate?',
+            'degree': 'What\'s your degree?',
+            'other_diet': 'We\'ll make sure you can enjoy all the food.',
+            'lennyface': 'Tip: you can chose from here <a href="http://textsmili.es/" target="_blank">'
                          ' http://textsmili.es/</a>',
             'projects': 'You can talk about about past hackathons, personal projects, awards etc. '
                         '(we love links) Show us your passion! :D',
@@ -192,7 +192,7 @@ class ApplicationForm(BetterModelForm):
         }
 
         labels = {
-            'gender': 'What gender do you associate with?',
+            'gender': 'What is your gender?',
             'graduation_year': 'What year are you graduating?',
             'tshirt_size': 'What\'s your t-shirt size?',
             'diet': 'Dietary requirements',
@@ -201,7 +201,7 @@ class ApplicationForm(BetterModelForm):
             'description': 'Why are you excited about %s?' % settings.HACKATHON_NAME,
             'projects': 'What projects have you worked on?',
             'resume': 'Upload your resume',
-            'reimb_amount': 'How much money (%s) would you need to afford traveling to %s?' % (
+            'reimb_amount': 'How much money (%s) would you need to afford travelling to %s?' % (
                 settings.CURRENCY, settings.HACKATHON_NAME),
 
         }
