be4979844d14b1ca8fd65a38291b45da764e18a1
Paul Horton <pahorton@asu.edu>
2019-09-14 20:36:51 -0700

    Added sponsor export

--- applications/admin.py
+++ applications/admin.py
@@ -9,7 +9,11 @@ from applications import models
 import csv
 from django.http import HttpResponse
 
-EXPORT_CSV_FIELDS = ['name', 'email', 'phone_number', 'university', 'gender','other_gender','ethnicity','other_ethnicity','degree', 'education', 'graduation_year', 'data_consent', 'referral']
+EXPORT_CSV_FIELDS = ['name', 'email', 'phone_number', 'university', 'gender','other_gender','ethnicity','other_ethnicity','degree', 'education', 'graduation_year', 'data_consent', 'referral','resume','sponsor_consent']
+
+EXPORT_CSV_FOR_MLH = ['name', 'email', 'phone_number', 'university', 'gender','other_gender','ethnicity','other_ethnicity','degree', 'education', 'graduation_year', 'data_consent']
+
+EXPORT_CSV_FOR_SPONSOR = ['name', 'university', 'degree', 'education', 'graduation_year', 'resume', 'sponsor_consent']
 
 EXPORT_CSV_FOR_SENDY = ['name','email']
 
@@ -18,52 +22,47 @@ class ApplicationAdmin(admin.ModelAdmin):
     list_display = ('user', 'name', 'votes', 'reimb', 'status',
                     'status_last_updated', 'diet')
     list_filter = ('status', 'first_timer', 'reimb', 'graduation_year',
-                   'university', 'origin', 'under_age', 'diet')
+                   'university', 'origin', 'under_age', 'diet','sponsor_consent')
     list_per_page = 200
     search_fields = ('user__name', 'user__email',
                      'description',)
     ordering = ('submission_date',)
     date_hierarchy = 'submission_date'
 
-    actions = ["export_as_sendy","export_as_csv"]
-    def export_as_sendy(self, request, queryset):
-        meta = self.model._meta
+    actions = ["export_as_csv", "export_as_sendy", "export_as_MLH", "export_as_sponsor"]
+
+    def export_csv(self, request, queryset, filename, fields):
 
         response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(filename)
         writer = csv.writer(response)
 
-        writer.writerow(EXPORT_CSV_FOR_SENDY)
+        writer.writerow(fields)
         for obj in queryset:
             row = []
-            for field in EXPORT_CSV_FOR_SENDY:
+            for field in fields:
                 if hasattr(getattr(obj,'user'),field):
                     row.append(getattr(getattr(obj,'user'),field))
                 else:
                     row.append(getattr(obj,field))
             written = writer.writerow(row)
-
         return response
-    export_as_sendy.short_description = "Export to Sendy CSV"
 
     def export_as_csv(self, request, queryset):
-        meta = self.model._meta
+        return self.export_csv(request,queryset,"hackers",EXPORT_CSV_FIELDS)
+    export_as_csv.short_description = "Export to All CSV"
 
-        response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
-        writer = csv.writer(response)
+    def export_as_sendy(self, request, queryset):
+        return self.export_csv(request,queryset,"sendy",EXPORT_CSV_FOR_SENDY)
+    export_as_sendy.short_description = "Export to Sendy CSV"
 
-        writer.writerow(EXPORT_CSV_FIELDS)
-        for obj in queryset:
-            row = []
-            for field in EXPORT_CSV_FIELDS:
-                if hasattr(getattr(obj,'user'),field):
-                    row.append(getattr(getattr(obj,'user'),field))
-                else:
-                    row.append(getattr(obj,field))
-            written = writer.writerow(row)
-        return response
-    export_as_csv.short_description = "Export to CSV"
+    def export_as_MLH(self, request, queryset):
+        return self.export_csv(request,queryset,"mlh",EXPORT_CSV_FOR_MLH)
+    export_as_MLH.short_description = "Export to MLH CSV"
+
+    def export_as_sponsor(self, request, queryset):
+        return self.export_csv(request,queryset,"sponsor",EXPORT_CSV_FOR_SPONSOR)
+    export_as_sponsor.short_description = "Export to Sponsor CSV"
 
     def name(self, obj):
         return obj.user.get_full_name() + ' (' + obj.user.email + ')'
